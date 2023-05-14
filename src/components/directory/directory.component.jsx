8c230ee4453d9b2a41036891ba5393ce55525240
Fathan Mubarok <fathan.mubarok@digits.id>
2023-05-14 07:30:32 +0700

    Update App and Create Component Directory

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,16 @@
+import CategoryItem from "../category-item/category-item.component";
+import "./directory.styles.scss";
+
+const Directory = ({ categories }) => {
+  return (
+    <>
+      <div className="categories-container">
+        {categories.map((categories) => (
+          <CategoryItem key={categories.id} category={categories} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Directory;
